5b4b706603083479304c7690eab85bdf1251a967
geonyoungkim <c2619zz@naver.com>
2018-01-23 17:10:58 +0900

    스케쥴 작성 Activity, 완료된 스케쥴 기간별 리스트 Activity

--- app/src/main/java/com/edge/weather/unithon/MainActivity.java
+++ app/src/main/java/com/edge/weather/unithon/MainActivity.java
@@ -1,7 +1,11 @@
 package com.edge.weather.unithon;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.test_write).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, WriteActivity.class);
+                startActivityForResult(i, 1);
+            }
+        });
+
+        findViewById(R.id.test_schedule_list).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,ScheduleListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                ScheduleVO  result=(ScheduleVO) data.getParcelableExtra("schedule");
+                Log.d("스케쥴",result.getContent()+"--"+result.getTitle());
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //만약 반환값이 없을 경우의 코드를 여기에 작성하세요.
+            }
+        }
+    }//onActivityResult
+
 }
--- app/src/main/java/com/edge/weather/unithon/ScheduleListActivity.java
+++ app/src/main/java/com/edge/weather/unithon/ScheduleListActivity.java
@@ -2,12 +2,121 @@ package com.edge.weather.unithon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class ScheduleListActivity extends AppCompatActivity {
+    /*private Map<String,List<ScheduleVO>> schedule_list_completed;
+    private List<ScheduleVO> applicable_period_schedule_list;*/
+    private Map<String,List<String>> schedule_list_completed;
+    private List<String> applicable_period_schedule_list;
+    ListView period_lv;
+    ListView schedule_lv;
+    CustomAdapter schedule_adapter;
+
+    List<String> period_list;
+    List<String> schedule_list=new ArrayList<String>();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_list);
+
+
+
+        schedule_list_completed=new HashMap<String,List<String>>();
+        applicable_period_schedule_list= new ArrayList<String>();
+
+        applicable_period_schedule_list.add("줄넘기 50개");
+        applicable_period_schedule_list.add("아침먹고 출근하기");
+        schedule_list_completed.put("2017/10/10~2017/12/15",applicable_period_schedule_list);
+
+        applicable_period_schedule_list=new ArrayList<String>();
+        applicable_period_schedule_list.add("영단어 50개 외우기");
+        applicable_period_schedule_list.add("백준 1개 풀기");
+        applicable_period_schedule_list.add("자격증 공부하기");
+        schedule_list_completed.put("2017/02/15~2017/02/25",applicable_period_schedule_list);
+
+        period_list=new ArrayList<String>(schedule_list_completed.keySet());
+        Collections.sort(period_list);
+        period_lv=findViewById(R.id.period_list);
+        schedule_lv=findViewById(R.id.schedule_list);
+
+        CustomAdapter period_adapter=new CustomAdapter(period_list,1);
+        period_lv.setAdapter(period_adapter);
+
+        schedule_adapter=new CustomAdapter(schedule_list,2);
+        schedule_lv.setAdapter(schedule_adapter);
+
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        List list;
+        int division;
+        public CustomAdapter(List list,int division){
+            this.division=division;
+            this.list=list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int i, View view, ViewGroup viewGroup) {
+            if(division==1){
+                view=getLayoutInflater().inflate(R.layout.row_period,null);
+                ((Button)view.findViewById(R.id.row_period)).setText(list.get(i).toString());
+                ((Button)view.findViewById(R.id.row_period)).setTag("true");
+                view.findViewById(R.id.row_period).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if("true".equals(view.findViewById(R.id.row_period).getTag().toString())){
+                            for(int j=0;j<schedule_list_completed.get(list.get(i)).size();j++){
+                                schedule_list.add(schedule_list_completed.get(list.get(i)).get(j).toString());
+
+                            }
+                            view.findViewById(R.id.row_period).setTag("false");
+                        }else{
+                            for(int j=0;j<schedule_list_completed.get(list.get(i)).size();j++){
+                                schedule_list.remove(schedule_list_completed.get(list.get(i)).get(j).toString());
+
+                            }
+                            view.findViewById(R.id.row_period).setTag("true");
+                        }
+
+                        schedule_adapter.notifyDataSetChanged();
+                    }
+                });
+            }else{
+                view=getLayoutInflater().inflate(R.layout.row_schedule,null);
+                ((TextView)view.findViewById(R.id.row_schedule)).setText(list.get(i).toString());
+            }
+            return view;
+        }
     }
 }
